f90c15c29fdfa215d08874d8c5e1f30b78866635
gmunu <oren@gmunu.org>
2014-10-05 20:09:29 -0700

    ex6: vectorized dataset3Params.m

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,34 +23,14 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-C_val = [0.01 0.03 0.1 0.3 1 3 10 30]';
-sigma_val = [0.01 0.03 0.1 0.3 1 3 10 30]';
-best_error = -1;
-
-for i = 1:length(C_val)
-    for j = 1:length(sigma_val)
-        C_i = C_val(i);
-        sigma_i = sigma_val(j);
-        model = svmTrain(X, y, C_i, @(x1, x2) gaussianKernel(x1, x2, sigma_i));
-        predictions = svmPredict(model, Xval);
-        prediction_error = mean(double(predictions ~= yval));
-        if best_error < 0
-            % first iteration
-            C = C_i;
-            sigma = sigma_i;
-            best_error = prediction_error;
-        elseif prediction_error < best_error
-            C = C_i;
-            sigma = sigma_i;
-            best_error = prediction_error;
-        end
-        fprintf('\nC: %f', C_i);
-        fprintf('\nsigma: %f', sigma_i);
-        fprintf('\ncurrent error: %f', prediction_error);
-        fprintf('\nbest error: %f', best_error);
-    end
-end
-fprintf('\n');
+C_vals = [0.01 0.03 0.1 0.3 1 3 10 30]';
+sigma_vals = [0.01 0.03 0.1 0.3 1 3 10 30]';
+C_mat = repmat(C_vals, size(sigma_vals'));
+sigma_mat = repmat(sigma_vals', size(C_vals));
+predictions = arrayfun(@(C_i, sigma_i) (mean(double(yval ~= (svmPredict(svmTrain(X, y, C_i, @(x1, x2) gaussianKernel(x1, x2, sigma_i)), Xval))))), C_mat, sigma_mat);
+[dummy, ix] = min(predictions(:));
+C = C_mat(ix);
+sigma = sigma_mat(ix);
 
 % =========================================================================
 
