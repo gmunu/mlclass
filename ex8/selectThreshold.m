dd4440ce908194dad8bf2a870bc93b257737673b
gmunu <oren@gmunu.org>
2014-10-09 20:03:10 -0700

    ex8: completely vectorized selectThreshold

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -11,7 +11,18 @@ bestF1 = 0;
 F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
-for epsilon = min(pval):stepsize:max(pval)
+epsilon_candidates = min(pval):stepsize:max(pval);
+predictions = bsxfun(@lt, pval, epsilon_candidates);
+TP = sum(bsxfun(@and, predictions, yval));
+FP = sum(bsxfun(@and, predictions, !yval));
+FN = sum(bsxfun(@and, !predictions, yval));
+% TN = sum(bsxfun(@and, !predictions, !yval));
+% R=TP/(TP+FN); P=TP/(TP+FP); F1=2RP/(R+P)=2TP/(2TP+FP+FN)
+F1 = 2 * TP ./ (2 * TP + FP + FN);
+[bestF1, ix] = max(F1);
+bestEpsilon = epsilon_candidates(ix);
+
+% for epsilon = min(pval):stepsize:max(pval)
     
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
@@ -23,20 +34,20 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-    pred = (pval < epsilon);
-    TP = sum(yval & pred);
-    FN = sum(yval & !pred);
-    FP = sum(!yval & pred);
-    TN = sum(!yval & !pred);
+    % pred = (pval < epsilon);
+    % TP = sum(yval & pred);
+    % FP = sum(!yval & pred);
+    % FN = sum(yval & !pred);
+    % TN = sum(!yval & !pred);
     % R=TP/(TP+FN); P=TP/(TP+FP); F1=2RP/(R+P)=2TP/(2TP+FP+FN)
-    F1 = 2 * TP / (2 * TP + FP + FN);
+    % F1 = 2 * TP / (2 * TP + FP + FN);
 
     % =============================================================
 
-    if F1 > bestF1
-       bestF1 = F1;
-       bestEpsilon = epsilon;
-    end
-end
+    % if F1 > bestF1
+       % bestF1 = F1;
+       % bestEpsilon = epsilon;
+    % end
+% end
 
 end
